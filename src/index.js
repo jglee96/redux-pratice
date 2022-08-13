58c253d4b7e562b2adc28152314dd5477f32f1c7
jglee96 <zakklee96@gmail.com>
2022-08-13 21:24:36 +0900

    [feat] dispatch
    * reducer 함수 안에서만 state를 수정할 수 있음
    * 이를 가능하게 하는 것이 parameter로 있는 action
    * store의 dispatch로 action object를 주고
    * reducer에서 action object에 따라 state를 수정함

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = legacy_createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
