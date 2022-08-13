a548fc7f2f29e17fc5afaf59b465e5e7ffcd4473
jglee96 <zakklee96@gmail.com>
2022-08-14 01:06:14 +0900

    [feat] delete todo
    * mutate 없이 delte 기능 구현
    * splice는 mutate를 하기 때문에 filter로 new array를 return하는 방식
    사용

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -35,7 +35,7 @@ const dispatchAddToDo = (text) => {
 };
 
 const dispatchDeleteToDo = (e) => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
