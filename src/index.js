16779d7836d8d926537aaac3faa2934010c661f7
jglee96 <zakklee96@gmail.com>
2022-08-14 00:48:27 +0900

    [feat] add ToDo
    * add ToDo action 구현
    * state는 single source 원칙이며, read-only이다
    * state는 mutate하지 않아야 한다.
    * array state에 새로운 element를 추가해야 하는 경우 기존 state에
    push하지 말고 새로운 array를 생성해야 한다.
    * state의 change를 감지하는데 push는 array를 change하지 않는다.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text }];
     case DELETE_TODO:
       return [];
     default:
@@ -19,6 +19,8 @@ const reducer = (state = [], action) => {
   }
 };
 const store = legacy_createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
   e.preventDefault();
