ebcea48eaab09f92aaa4e63a7b697f5aa0e525be
jglee96 <zakklee96@gmail.com>
2022-08-14 18:16:46 +0900

    [feat] Detail
    * list에서 ToDo 선택하면 detail 창으로 넘어가면서 해당 todo 정보만
    보이는 Detail component 구현
    * react-router가 update되면서 Link가 props를 전달하는 기능이 사라짐
    (https://reactrouter.com/docs/en/v6/upgrading/v5#remove-link-component-prop)
    * 따라서 Detail의 mapStateToProp에서 ownProps가 아닌 useParams로 id
    정보를 가져오도록 구현함

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { actionCreators } from "../store";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+  const params = useParams();
+  const detail = toDos.find((toDo) => toDo.id === parseInt(params.id));
+  return (
+    <>
+      <h1>{detail.text}</h1>
+      <h5>Created at: {detail.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
