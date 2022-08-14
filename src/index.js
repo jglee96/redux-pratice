4d36d967bc1fa8298e6aceb34c22d5b8aec54dd6
jglee96 <zakklee96@gmail.com>
2022-08-14 15:10:10 +0900

    [feat] connect react and redux
    * store.js 에 redux part 구현 -> store를 export함
    * index.js에서 react-redux의 Provider로 App component를 감쌈
    * Provider의 prop으로 store를 줌
    * react 사용 이전에는 store의 subscribe로 state가 update 될 때마다
    render를 해주는 함수를 직접 구현하여 연결해줌
    * react에서 render 하는 역할을 하기 때문에 이를 연결해주는 것이
    react-redux가 아닐까 생각됨

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App></App>);
+root.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>
+);
