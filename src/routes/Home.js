d3ac82407b73190c96c97430940eee5d5e0158ee
jglee96 <zakklee96@gmail.com>
2022-08-14 18:59:16 +0900

    [refactor] createSlice
    * createAction, createReducer를 createSlice 하나로 할 수 있음

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import ToDo from "../components/ToDo";
-import { actionCreators } from "../store";
+import { add } from "../store";
 
 function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
@@ -35,6 +35,6 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return { addToDo: (text) => dispatch(actionCreators.addToDo(text)) };
+  return { addToDo: (text) => dispatch(add(text)) };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -1,19 +1,21 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+const store = configureStore({ reducer: toDos.reducer });
+
+console.log(toDos.actions);
+
+export const { add, remove } = toDos.actions;
+
 export default store;
