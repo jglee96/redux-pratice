695e16ee21620a7e20e6e4a1f2067e5d160b7b8c
jglee96 <zakklee96@gmail.com>
2022-08-14 18:41:55 +0900

    [refactor] createReducer
    * reducer를 간단하게 해주는 createReducer
    * state를 mutate할 수 있음 -> 뒤에 있는  toolkit, immer에서 new state로
    변환하여 return 해줌
    * new state를 만들어서 return 해줄 수도 있음 -> mutate state를 return
    하지 않기만 하면 됨

--- src/store.js
+++ src/store.js
@@ -1,19 +1,27 @@
 import { legacy_createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = legacy_createStore(reducer);
 export const actionCreators = {
