134d5ca5fe0af5e72307a31465c444e046e31ddb
jglee96 <zakklee96@gmail.com>
2022-08-14 18:49:46 +0900

    [chore] configureStore
    * createStore 대신에 configureStore 사용
    * reducer를 object 형태로 전달해주는 거 빼고는 다른게 없음
    * 대신 브라우저에서 'Redux DevTools'로 분석가능
    * redux에서도 createStore를 deprecated, legacy로 하고 congifureStore
    사용을 권장하고 있음

--- src/store.js
+++ src/store.js
@@ -1,19 +1,7 @@
-import { legacy_createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
 
 const reducer = createReducer([], {
   [addToDo]: (state, action) => {
@@ -23,7 +11,7 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = legacy_createStore(reducer);
+const store = configureStore({ reducer });
 export const actionCreators = {
   addToDo,
   deleteToDo,
