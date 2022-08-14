37dd9d3a97b51a2d2d8b240496dc926a135d2b8e
jglee96 <zakklee96@gmail.com>
2022-08-14 14:57:07 +0900

    [feat] React ToDo List setup
    * React에서 Redux 사용을 연습하기 위한 ToDo list setup
    * 강의에서는 HashRouter를 사용하였지만 최신 업데이트 내용에 맞춰
    BrowerRouter를 사용하도록 수정
    * HashRouter
     * 지원되는 브라우저나 웹 서버에 제한이 없다.
     * 주소에 해쉬(#)가 붙는다.
     * 검색 엔젠이 읽지 못한다.
     * URL 해쉬를 서버에서 읽을 수 없기 때문에 SSR로 설정을 백업할 수 없다.
    해쉬 히스토리를 지원하지 않는다.
    * BrowserROuter
     * 레거시 브라우저(IE9 이하)에서는 사용할 수 없다.
     * 히스토리를 지원한다.
    * 이저에는 URL위치 변경에 js를 사용할 수 없어 HashRouter를 사용했다.
    그러나 BrowserRouter가 나오면서 HashRouter를 더 이상 사용하지
    않게되었다.
    * Route
     * 이전에는 exact prop을 주어 복수의 라우팅을 막았지만, 이제 exact가
    기본이 되고 복수의 라우팅을 하고 싶은 경우 URL 뒤에 *을 붙여서 사용한다.
    * component 대신 element로 Component를 전달한다.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
